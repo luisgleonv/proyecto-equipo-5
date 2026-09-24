73b32cf8351725527a7fdf5d842a47d4e0170f80
Luis Guillermo Leon Vargas <luisgleonv@gmail.com>
2026-09-23 20:03:30 -0600

    Agregamos el configmap, se adapto el script de instalación a ello

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,6 +39,8 @@ echo "7. Desplegando Constraints (Reglas activas, auditoría y excepciones)..."
 find manifests -type f -name "*constraint*.yaml" -exec kubectl apply -f {} \;
 
 echo "8. Desplegando Gatekeeper Policy Manager (Dashboard GPM)..."
+# Inyectar el ConfigMap antes del dashboard
+kubectl apply -f manifests/configmap.yaml
 kubectl apply -f manifests/gpm.yaml
 
 echo "========================================================"
